a55150a0dce6724adfca18c442d1c2148a8d2c98
laurencekeithabaring <72242203+laurencekeithabaring@users.noreply.github.com>
2020-10-27 14:12:06 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<style>
+svg text {
+  stroke: #ee0;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 16;
+  filter: drop-shadow(2px 2px 2px #a20);
+}
+body{
+  background: #000;
+  color: #ddd;
+}
+</style>
+
+<svg viewBox="0 0 700 300">
+  <defs>
+    <path id="MyPath"
+          fill="white" stroke="white" stroke-width="4" d="M66.039,133.545c0,0-21-57,18-67s49-4,65,8
+    s30,41,53,27s66,4,58,32s-5,44,18,57s22,46,0,45s-54-40-68-16s-40,88-83,48s11-61-11-80s-79-7-70-41
+    C46.039,146.545,53.039,128.545,66.039,133.545z" pathLength="1">
+    <linearGradient id="grad">      
+    </linearGradient>
+  </defs>
+
+  <text fill="url(#grad)" textLength="500" lengthAdjust="spacingAndGlyphs">
+    <textPath xlink:href="#MyPath"  method="stretch" spacing="auto">
+     Many of life’s failures are people who did not realize how close they were to success when they gave up.
+    </textPath>
+  </text>
+</svg>
